80dfbfb30b7c83c052f8612597ba788045231c22
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-08-04 15:45:57 +0300

    Init project

---
+++ app/dashboard/page.js
@@ -0,0 +1,25 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/libs/next-auth";
+import connectMongo from "@/libs/mongoose";
+import User from "@/models/User";
+
+export default async function Dashboard() {
+  await connectMongo();
+  const session = await getServerSession(authOptions);
+  const user = await User.findById(session.user.id);
+
+  return (
+    <>
+      <main className="min-h-screen p-8 pb-24">
+        <section className="max-w-xl mx-auto space-y-8">
+          <h1 className="text-3xl md:text-4xl font-extrabold">
+            User Dashboard
+          </h1>
+          <p>Welcome {user.name} 👋</p>
+          <p>Your email is {user.email}</p>
+          <p>Your plan status is {user.hasAccess ? "Active" : "Inactive"}</p>
+        </section>
+      </main>
+    </>
+  );
+}
---
+++ config.js
@@ -0,0 +1,103 @@
+import themes from "daisyui/src/theming/themes";
+
+const config = {
+  // REQUIRED
+  appName: "ShipFast",
+  // REQUIRED: a short description of your app for SEO tags (can be overwritten)
+  appDescription:
+    "The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web app.",
+  // REQUIRED (no https://, not trialing slash at the end, just the naked domain)
+  domainName: "shipfa.st",
+  crisp: {
+    // Crisp website ID. IF YOU DON'T USE CRISP: just remove this => Then add a support email in this config file (resend.supportEmail) otherwise customer support won't work.
+    id: "",
+    // Hide Crisp by default, except on route "/". Crisp is toggled with <ButtonSupport/>. If you want to show Crisp on every routes, just remove this below
+    onlyShowOnRoutes: ["/"],
+  },
+  stripe: {
+    // Create multiple plans in your Stripe dashboard, then add them here. You can add as many plans as you want, just make sure to add the priceId
+    plans: [
+      {
+        // REQUIRED — we use this to find the plan in the webhook (for instance if you want to update the user's credits based on the plan)
+        priceId:
+          process.env.NODE_ENV === "development"
+            ? "price_1RsNEOHWilXYbGmnTE0WN3wq"
+            : "price_1RsNEOHWilXYbGmnTE0WN3wq",
+        //  REQUIRED - Name of the plan, displayed on the pricing page
+        name: "Starter",
+        // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
+        description: "Perfect for small projects",
+        // The price you want to display, the one user will be charged on Stripe.
+        price: 5.99,
+        // Euro price for display
+        priceEuro: 3.06,
+        // Add interval for subscriptions (e.g., "month", "year")
+        interval: "месец",
+        features: [
+          {
+            name: "NextJS boilerplate",
+          },
+          { name: "User oauth" },
+          { name: "Database" },
+          { name: "Emails" },
+        ],
+      },
+      {
+        // This plan will look different on the pricing page, it will be highlighted. You can only have one plan with isFeatured: true
+        isFeatured: true,
+        priceId:
+          process.env.NODE_ENV === "development"
+            ? "price_1RsNEOHWilXYbGmnGSXsXeH1"
+            : "price_1RsNEOHWilXYbGmnGSXsXeH1",
+        name: "Advanced",
+        description: "You need more power",
+        price: 59.99,
+        // Euro price for display
+        priceEuro: 30.61,
+        // Add interval for subscriptions (e.g., "month", "year")
+        interval: "година",
+        priceAnchor: 71.88,
+        priceAnchorEuro: 36.72,
+        features: [
+          {
+            name: "NextJS boilerplate",
+          },
+          { name: "User oauth" },
+          { name: "Database" },
+          { name: "Emails" },
+          { name: "1 year of updates" },
+          { name: "24/7 support" },
+        ],
+      },
+    ],
+  },
+  aws: {
+    // If you use AWS S3/Cloudfront, put values in here
+    bucket: "bucket-name",
+    bucketUrl: `https://bucket-name.s3.amazonaws.com/`,
+    cdn: "https://cdn-id.cloudfront.net/",
+  },
+  resend: {
+    // REQUIRED — Email 'From' field to be used when sending magic login links
+    fromNoReply: `E-menu <noreply@resend.e-menu.bg>`,
+    // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
+    fromAdmin: `E-menu <kzaraliev@resend.e-menu.bg>`,
+    // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
+    supportEmail: "support@resend.e-menu.bg",
+  },
+  colors: {
+    // REQUIRED — The DaisyUI theme to use (added to the main layout.js). Leave blank for default (light & dark mode). If you any other theme than light/dark, you need to add it in config.tailwind.js in daisyui.themes.
+    theme: "light",
+    // REQUIRED — This color will be reflected on the whole app outside of the document (loading bar, Chrome tabs, etc..). By default it takes the primary color from your DaisyUI theme (make sure to update your the theme name after "data-theme=")
+    // OR you can just do this to use a custom color: main: "#f37055". HEX only.
+    main: themes["light"]["primary"],
+  },
+  auth: {
+    // REQUIRED — the path to log in users. It's use to protect private routes (like /dashboard). It's used in apiClient (/libs/api.js) upon 401 errors from our API
+    loginUrl: "/api/auth/signin",
+    // REQUIRED — the path you want to redirect users after successfull login (i.e. /dashboard, /private). This is normally a private page for users to manage their accounts. It's used in apiClient (/libs/api.js) upon 401 errors from our API & in ButtonSignin.js
+    callbackUrl: "/dashboard",
+  },
+};
+
+export default config;
