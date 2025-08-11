5117389223e88f09113edc3a78c416f96ad4514d
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-08-11 15:31:29 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,49 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: [
-      // NextJS <Image> component needs to whitelist domains for src={}
-      "lh3.googleusercontent.com",
-      "pbs.twimg.com",
-      "images.unsplash.com",
-      "logos-world.net",
+    remotePatterns: [
+      // Google profile images
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Twitter/X profile images
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Unsplash images
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Logos World
+      {
+        protocol: 'https',
+        hostname: 'logos-world.net',
+        port: '',
+        pathname: '/**',
+      },
+      // Your specific S3 bucket
+      {
+        protocol: 'https',
+        hostname: 'e-menu-images-production.s3.eu-north-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      // Support for any S3 bucket in eu-north-1 region
+      {
+        protocol: 'https',
+        hostname: '*.s3.eu-north-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
