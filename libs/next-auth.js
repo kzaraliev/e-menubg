7d698c98fc42291bd1e7eca389cefda553649e39
Konstantin <106445847+kzaraliev@users.noreply.github.com>
2025-08-11 15:24:25 +0300

    Update next-auth.js

--- libs/next-auth.js
+++ libs/next-auth.js
@@ -29,8 +29,8 @@ export const authOptions = {
           EmailProvider({
             server: {
               host: "smtp.resend.com",
-              port: 465,
-              secure: true,
+              port: 587,
+              secure: false,
               auth: {
                 user: "resend",
                 pass: process.env.RESEND_API_KEY,
